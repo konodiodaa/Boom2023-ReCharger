320f36bb3e2529da92ae4219209b8f90beb2a05e
LAPTOP-OQ70P6DP\Lihao <chenlihao117@protonmail.com>
2023-05-12 15:51:51 -0700

    Lihao, implemented the wire collision physics simulation and the wire cut mechanics.

--- Assets/Scripts/RopePhysics/WireEulerIntegration.cs
+++ Assets/Scripts/RopePhysics/WireEulerIntegration.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
+using Unity.Mathematics;
+using System;
+using JetBrains.Annotations;
+using UnityEngine.Assertions;
 public class WireEulerIntegration : MonoBehaviour
 {
     public struct RopeSegment
@@ -18,10 +22,9 @@ public class WireEulerIntegration : MonoBehaviour
 
     }
 
-    private LineRenderer _lineRenderer;
-    private List<RopeSegment> _segments = new List<RopeSegment>();
     [SerializeField] private Transform _startPoint;
     [SerializeField] private Transform _endPoint;
+    [SerializeField] private LayerMask _collisionMask;
 
     [SerializeField] private float _width = 0.1f;
     [SerializeField] private Color _color = Color.black;
@@ -31,10 +34,29 @@ public class WireEulerIntegration : MonoBehaviour
 
     [SerializeField] private Vector2 _forceGravity = new Vector2(0f, -1.0f);
     [SerializeField] private int _solveStep = 30;
+    
+    private List<RopeSegment> _segments = new List<RopeSegment>();
+    private LineRenderer _cuttedLineRenderer;
+    private LineRenderer _lineRenderer;
+    
+    private int _wireCuttedIndex = -2;
+    public Action OnWireCut;
 
     private void Awake()
     {
-        _lineRenderer = GetComponent<LineRenderer>();
+        LineRenderer[] lineRenderers = GetComponentsInChildren<LineRenderer>();
+        if (lineRenderers[0].gameObject == this.gameObject)
+        {
+            _lineRenderer = lineRenderers[0];
+            _cuttedLineRenderer = lineRenderers[1];
+        }
+        else
+        {
+            _lineRenderer = lineRenderers[1];
+            _cuttedLineRenderer = lineRenderers[0];
+        }
+
+        
         _lineRenderer.positionCount = _segmentCount;
         _lineRenderer.startWidth = _width;
         _lineRenderer.endWidth = _width;
@@ -42,6 +64,16 @@ public class WireEulerIntegration : MonoBehaviour
         _lineRenderer.endColor = _color;
         _lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         
+        
+        
+        _cuttedLineRenderer.positionCount = 0;
+        _cuttedLineRenderer.startWidth = _width;
+        _cuttedLineRenderer.endWidth = _width;
+        _cuttedLineRenderer.startColor = _color;
+        _cuttedLineRenderer.endColor = _color;
+        _cuttedLineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+        Debug.Log(_lineRenderer.gameObject == _cuttedLineRenderer.gameObject);
     }
 
     private void Start()
@@ -60,14 +92,42 @@ public class WireEulerIntegration : MonoBehaviour
         Simulate();
         DrawRope();
     }
+   
     private void DrawRope()
     {
-        Vector3[] segmentPositions = new Vector3[_segmentCount];
-        for(int i = 0; i < _segmentCount; i++)
+        
+        if (_wireCuttedIndex == -2)
         {
-            segmentPositions[i] = _segments[i].CurrPos;
+            Vector3[] segmentPositions = new Vector3[_segmentCount];
+            for (int i = 0; i < _segmentCount; i++)
+            {
+                segmentPositions[i] = _segments[i].CurrPos;
+            }
+            _lineRenderer.SetPositions(segmentPositions);
         }
-        _lineRenderer.SetPositions(segmentPositions);
+        else
+        {
+            Vector3[] wireFirstPart = new Vector3[_wireCuttedIndex + 1];
+            Vector3[] wireSecondPart = new Vector3[_segmentCount - _wireCuttedIndex - 1];
+
+            _lineRenderer.positionCount = wireFirstPart.Length;
+            _cuttedLineRenderer.positionCount = wireSecondPart.Length;
+
+            for (int i = 0; i < _wireCuttedIndex + 1; i++)
+            {
+                wireFirstPart[i] = _segments[i].CurrPos;
+                
+            }
+            for(int j = _wireCuttedIndex + 1; j < _segmentCount; j ++)
+            {
+                wireSecondPart[j - (_wireCuttedIndex + 1)] = _segments[j].CurrPos;
+            
+            }
+
+            _lineRenderer.SetPositions(wireFirstPart);
+            _cuttedLineRenderer.SetPositions(wireSecondPart);
+        }
+
     }
 
     private void Simulate()
@@ -102,6 +162,7 @@ public class WireEulerIntegration : MonoBehaviour
             RopeSegment currSegment = _segments[i];
             RopeSegment nextSegment = _segments[i + 1];
 
+            //Rope 
             float dist = (currSegment.CurrPos - nextSegment.CurrPos).magnitude;
             float deviate = Mathf.Abs(dist - _segmentLength);
             Vector2 changeDir = Vector2.zero;
@@ -117,11 +178,85 @@ public class WireEulerIntegration : MonoBehaviour
 
             currSegment.CurrPos -= changeDir * deviate * 0.5f;
             nextSegment.CurrPos += changeDir * deviate * 0.5f;
-            _segments[i] = currSegment;
-            _segments[i + 1] = nextSegment;
 
+            if (i != _wireCuttedIndex)
+            {
+                _segments[i] = currSegment;
+                _segments[i + 1] = nextSegment;
+            }
+            //Collision Physics Simulation.
+            Collider2D hitCollider = Physics2D.OverlapCircle(currSegment.CurrPos, _width, _collisionMask);
+            if (hitCollider)
+            {
+                //Wire Cut
+                WireCutter cutter = hitCollider.gameObject.GetComponent<WireCutter>();
+                if(cutter && _wireCuttedIndex == -2)
+                {
+                    _wireCuttedIndex = i;
+                    OnWireCut?.Invoke();
+                    Debug.Log("Wire got cutted");
+                }
+
+                Transform hitTransform = hitCollider.transform;
+                if (hitCollider is CircleCollider2D)
+                {
+                    CircleCollider2D circleCollider = (CircleCollider2D)hitCollider;
+                    float hitColliderRadius = circleCollider.radius * hitTransform.lossyScale.x;
+                    Vector2 hitColliderCenter = hitTransform.position;
+                    Vector2 hitResolvePos = hitColliderCenter + (currSegment.CurrPos - hitColliderCenter).normalized * (hitColliderRadius + _width);
+
+                    //Debug.DrawLine(p.posCurrent, hitResolvePos);
+                    currSegment.CurrPos = hitResolvePos;
+                }
+                else if (hitCollider is BoxCollider2D)
+                {
+                    BoxCollider2D boxCollider = (BoxCollider2D)hitCollider;
+                    Vector2 boxColliderSize = boxCollider.size * hitTransform.lossyScale;
+                    Vector2 hitColliderCenter = hitTransform.position;
+
+                    Vector2 hitResolvePos = currSegment.CurrPos;
+                    Vector2 boxMin = hitColliderCenter - boxColliderSize / 2 - Vector2.one * _width;
+                    Vector2 boxMax = hitColliderCenter + boxColliderSize / 2 + Vector2.one * _width;
+
+                    float moveRight = boxMax.x - currSegment.CurrPos.x;
+                    float moveLeft  = -boxMin.x + currSegment.CurrPos.x;
+                    float moveTop   = boxMax.y - currSegment.CurrPos.y;
+                    float moveDown  = -boxMin.y + currSegment.CurrPos.y;
+
+                    float moveMin = math.min(math.min(moveRight, moveLeft), math.min(moveTop, moveDown));
+                    if (moveMin == moveRight)
+                    {
+                        hitResolvePos.x += moveRight;
+                    }
+                    else if (moveMin == moveLeft)
+                    {
+                        hitResolvePos.x -= moveLeft;
+                    }
+                    else if (moveMin == moveTop)
+                    {
+                        hitResolvePos.y += moveTop;
+                    }
+                    else
+                    {
+                        hitResolvePos.y -= moveDown;
+                    }
+
+                    //Debug.DrawLine(p.posCurrent, hitResolvePos);
+                    currSegment.CurrPos = hitResolvePos;
+                }
+                //Update Again.
+                _segments[i] = currSegment;
+
+
+            }
         }
     }
-
+    public void OnDrawGizmos()
+    {
+        for (int i = 0; i < this._segments.Count; i++)
+        {
+            Gizmos.DrawSphere(_segments[i].CurrPos, _width / 2);
+        }
+    }
 
 }
